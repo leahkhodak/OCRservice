050240fc33e2e6854887673d0b75eef4e4f9d566
Leah Khodak <leah.khodak@gmail.com>
2017-07-18 19:59:00 -0400

    added income object

---
+++ public/javascripts/incomeObject.js
@@ -0,0 +1,10 @@
+
+incomeJSON = new Object();
+
+var income;
+
+function IncomeJSON(income) {
+  this.income = income;
+};
+
+module.exports = IncomeJSON;
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var detectText = require('../public/javascripts/detectText.js');
+var incomeJSON = require('../public/javascripts/incomeObject.js');
 
 var router = express.Router();
 // The path to the local image file
 const fileName = './public/images/t4-1.jpeg';
 
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next){
-  detectText(fileName, function(err, income){
+  detectText(fileName, function(income){
     console.log("inside");
-    if(err != null){
-      console.log(err);
-    }
-    else{
     console.log("income: "+income);
-    res.send(income);
-    }
+    var responseIncome = new incomeJSON(income);
+    res.send(responseIncome);
   });
 
 });
